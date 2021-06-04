6886a485937562750bf9adb41a2be55ce20d7612
mohamed salah <mohamed.salah5369@gmail.com>
2021-06-04 15:16:58 +0200

    task 1 solution
    reading data from CSV file, parse it and store it in the list of Pyramid objects

---
+++ Pyramids/src/pyramids/Pyramids.java
@@ -0,0 +1,28 @@
+package pyramids;
+
+
+import PyramidsPKG.Pyramid;
+import PyramidsPKG.PyramidCSVDAO;
+import java.util.Iterator;
+import java.util.List;
+
+public class Pyramids {
+
+    public static void main(String[] args) {
+        
+        String path = "E:\\iti\\10 - JAVA _ UML\\03 - exercise\\Pyramids\\pyramids.csv";
+        
+        PyramidCSVDAO dataset = new PyramidCSVDAO();
+        List<Pyramid> d = dataset.getData(path);
+        
+        for (Iterator<Pyramid> iterator = d.iterator(); iterator.hasNext();) {
+            Pyramid next = iterator.next();
+            System.out.println(next);
+            
+        }
+        
+        
+        
+    }
+    
+}
